ac6c8c78471a69d4fc281d1ae26d09eb2ad6b6c1
Jeremy Felt <jeremy.felt@gmail.com>
2016-12-15 13:53:16 -0800

    Add custom Content Visibility groups for IR

--- functions.php
+++ functions.php
@@ -98,3 +98,76 @@ function ir_set_auth_cookies( $user, $user_ad_data ) {
 
 	setcookie( 'ir_ad_groups', $user_ad_groups, $expire, SITECOOKIEPATH, COOKIE_DOMAIN, $secure, true );
 }
+
+add_filter( 'content_visibility_default_groups', 'ir_add_visibility_groups', 99 );
+/**
+ * Extends the list of groups offered in the Content Visibility plugin.
+ *
+ * @param array $groups
+ *
+ * @return array
+ */
+function ir_add_visibility_groups( $groups ) {
+	$ir_groups = array(
+		array( 'id' => 'ir-managers', 'name' => 'IR Managers' ),
+		array( 'id' => 'ir-web-users', 'name' => 'IR Web Users' ),
+		array( 'id' => 'ir-admin', 'name' => 'IR Admin' ),
+		array( 'id' => 'ir-hrs-aa', 'name' => 'HRS Appointing Authority' ),
+	);
+	$groups = array_merge( $groups, $ir_groups );
+
+	return $groups;
+}
+
+add_filter( 'user_in_content_visibility_groups', 'ir_process_content_visibility_permissions', 10, 3 );
+/**
+ * Determines if a given user is a member of the passed groups.
+ *
+ * @param bool  $allowed Whether the user is associated with the passed groups.
+ * @param int   $user_id ID of the user.
+ * @param array $groups  List of groups to check the user against.
+ *
+ * @return bool False if the user is not a group member. True if the user is.
+ */
+public function ir_process_content_visibility_permissions( $allowed, $user_id, $groups ) {
+	// Don't override a previous success.
+	if ( true === $allowed ) {
+		return $allowed;
+	}
+
+	// The WSUWP SSO Authentication plugin is required for this check to work.
+	if ( ! class_exists( 'WSUWP_SSO_Authentication' ) ) {
+		return $allowed;
+	}
+
+	if ( 'nid' !== WSUWP_SSO_Authentication()->get_user_type( $user_id ) ) {
+		return $allowed;
+	}
+
+	// Skip any further checking if AD is disabled locally.
+	if ( defined( 'WSU_LOCAL_CONFIG' ) && WSU_LOCAL_CONFIG && false === apply_filters( 'wsuwp_sso_force_local_ad', false ) ) {
+		return $allowed;
+	}
+
+	$user = new WP_User( $user_id );
+
+	$user_ad_data = WSUWP_SSO_Authentication()->refresh_user_data( $user );
+
+	if ( in_array( 'ir-managers', $groups, true ) && in_array( 'IR_Managers', $user_ad_data['memberof'], true ) ) {
+		return true;
+	}
+
+	if ( in_array( 'ir-web-users', $groups, true ) && in_array( 'IR_Web_Users', $user_ad_data['memberof'], true ) ) {
+		return true;
+	}
+
+	if ( in_array( 'ir-admin', $groups, true ) && in_array( 'IR_Admin', $user_ad_data['memberof'], true ) ) {
+		return true;
+	}
+
+	if ( in_array( 'ir-hrs-aa', $groups, true ) && in_array( 'HRS_Appointing_Authority', $user_ad_data['memberof'], true ) ) {
+		return true;
+	}
+
+	return $allowed;
+}

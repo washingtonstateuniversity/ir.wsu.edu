8e9b0d6097927d49ff2484ecc7f6f7c7fa3b4a07
Nic Ford <nicdford@gmail.com>
2020-06-24 09:52:05 -0700

    Force all json to be in the theme

--- functions.php
+++ functions.php
@@ -80,7 +80,7 @@ function display_ir_college_dept_profiles( $atts ) {
 	if ( ! isset( $atts['json_url'] ) ) {
 		$json_url = get_stylesheet_directory_uri() . '/js/menu.json';
 	} else {
-		$json_url = $atts['json_url'];
+		$json_url = get_stylesheet_directory_uri() . $atts['json_url'];
 	}
 
 	if ( ! isset( $atts['suffix'] ) ) {
